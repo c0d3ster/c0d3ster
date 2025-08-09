e817b2671fce5db5cd24720d884f78e73a63bf10
cody douglass <c0d3ster@vt.edu>
2025-08-09 01:07:08 -0400

    refactor: finalize atomic design by cleaning up old files

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,5 @@
 // Atomic Design Structure
 export * from './atoms'
-// Legacy exports (for compatibility during transition)
-export * from './Contact'
-export * from './Header'
-export * from './LocaleSwitcher'
-
-export * from './MatrixHero'
 export * from './molecules'
 export * from './organisms'
-export * from './Portfolio'
 export * from './templates'
