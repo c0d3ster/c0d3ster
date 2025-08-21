00c040cf5a4ed72c26da3e0bd7fac91d96c79d49
cody douglass <c0d3ster@vt.edu>
2025-08-21 19:07:40 -0400

    chore: fix unused dependencies on run all and pglite

--- src/components/molecules/project-card/index.ts
+++ src/components/molecules/project-card/index.ts
@@ -1,4 +1,3 @@
 export { ProjectCard } from './ProjectCard'
 export type { Project } from './ProjectCard'
 export { ProjectStatusCard } from './ProjectStatusCard'
-export type { ProjectStatusCardProps } from './ProjectStatusCard'
