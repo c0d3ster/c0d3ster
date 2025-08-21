4058b1805515619c25d881758339074153c20602
cody douglass <c0d3ster@vt.edu>
2025-08-20 21:19:55 -0400

    fix: remove more internationalization

--- knip.config.ts
+++ knip.config.ts
@@ -2,7 +2,7 @@ import type { KnipConfig } from 'knip'
 
 const config: KnipConfig = {
   // Files to exclude from Knip analysis
-  ignore: ['src/libs/I18n.ts', 'src/types/I18n.ts', 'tests/**/*.ts'],
+  ignore: ['tests/**/*.ts'],
   // Dependencies to ignore during analysis
   ignoreDependencies: [
     '@commitlint/types',
