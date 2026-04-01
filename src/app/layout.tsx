d05c56878a980b24352f9a4742e44bffa7dfd38b
cody douglass <c0d3ster@vt.edu>
2026-04-01 14:14:41 -0400

    feat: vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 
 import { ApolloProvider } from '@apollo/client/react'
 import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 import { PostHogProvider } from '@/analytics'
 import { ToastContainer } from '@/components/atoms'
@@ -24,6 +25,7 @@ export default function RootLayout({
                 {children}
                 <ToastContainer />
                 <Analytics />
+                <SpeedInsights />
               </QueryProvider>
             </ApolloProvider>
           </PostHogProvider>
